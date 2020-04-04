f585194770e629153c72cd8b93b83701eec3e2f0
dharmveer97 <dvbangar8@gmail.com>
2020-04-04 12:24:00 +0530

    minor fixes

--- schemas/vehicleHirePage.js
+++ schemas/vehicleHirePage.js
@@ -22,9 +22,6 @@ export default {
       title: 'Slug',
       name: 'slug',
       type: 'slug',
-      options: {
-        source: 'hireOptionTitle',
-      },
     },
     {
       name: 'hireOptionSubtitle',
