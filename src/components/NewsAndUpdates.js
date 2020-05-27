94e8d15f08b1f3884d5ed9e81210f3ead4a128c4
dharmveer97 <dvbangar8@gmail.com>
2020-05-27 15:45:54 +0530

    minor fixes.

--- src/components/NewsAndUpdates.js
+++ src/components/NewsAndUpdates.js
@@ -1,50 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import NewsCard from './NewsCard';
 
-import ImageWithDetails from './elements/ImageWithDetails';
-
-const Data = [
-  {
-    subtitle:
-      "Many of us are working on a computer desktop that when it comes to purchase a new computer , we don't consider",
-    title: 'Why Do You Need To Join An Affiliate Marketing Network',
-    date: '27 May , 2019',
-    source:
-      'https://images.unsplash.com/photo-1544380935-17ce6a107313?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-  },
-  {
-    subtitle:
-      "Many of us are working on a computer desktop that when it comes to purchase a new computer , we don't consider",
-    title: 'Why Do You Need To Join An Affiliate Marketing Network',
-    date: '27 May , 2019',
-    source:
-      'https://images.unsplash.com/photo-1544380935-17ce6a107313?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-  },
-  {
-    subtitle:
-      "Many of us are working on a computer desktop that when it comes to purchase a new computer , we don't consider",
-    title: 'Why Do You Need To Join An Affiliate Marketing Network',
-    date: '27 May , 2019',
-    source:
-      'https://images.unsplash.com/photo-1544380935-17ce6a107313?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-  },
-];
+const Header = styled.header`
+  margin-bottom: 2rem;
+`;
 
 const NewsAndUpdates = () => {
   return (
-    <div className="section">
+    <div className="hero section is-block is-relative">
       <div className="container">
-        <h1 className="title is-3 has-text-centered">News and Updates</h1>
-        <div className="columns is-centered">
-          {Data.map(data => (
-            <div className="column is-4">
-              <ImageWithDetails
-                subtitle={data.subtitle}
-                title={data.title}
-                date={data.date}
-                source={data.source}
-              />
-            </div>
-          ))}
+        <Header className="">
+          <h1 className="title is-3 has-text-centered ">News and Updates</h1>
+        </Header>
+        <div className="columns is-multiline">
+          <NewsCard />
+          <NewsCard />
+          <NewsCard />
         </div>
       </div>
     </div>
---
+++ src/components/NewsCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+  .card {
+    border-radius: 8px;
+    box-shadow: none;
+  }
+  img {
+    border-radius: 8px;
+  }
+`;
+
+const NewsCard = () => {
+  return (
+    <CardWrapper className="column is-4">
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src="https://bulma.io/images/placeholders/1280x960.png"
+              alt="Place"
+            />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-4">John Smith</p>
+              <p className="subtitle is-6">@johnsmith</p>
+            </div>
+          </div>
+          <div className="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            nec iaculis mauris. <a>@bulmaio</a>.<a href="#">#css</a>{' '}
+            <a href="#">#responsive</a>
+            <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+      </div>
+    </CardWrapper>
+  );
+};
+export default NewsCard;
