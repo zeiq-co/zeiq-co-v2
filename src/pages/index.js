2672ee3a839e6d9a443762794c82a02837d06205
dharmveer97 <dvbangar8@gmail.com>
2020-05-27 23:55:03 +0530

    minor improvements.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,28 +7,27 @@ import Work from '../components/Work';
 import SocialStuff from '../components/SocialStuff';
 import NewsAndUpdates from '../components/NewsAndUpdates';
 
-export default class IndexPage extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <Seo title="Home" description="Welcome to GatsbyJs v1" />
-        <HomeHero />
-        <Work />
-        <section className="section">
-          <div className="container">
-            <div className="columns is-multiline">
-              <OurProcess text="Requirement Analysis" sideImage />
-              <OurProcess sideImage text="Project Allocation" />
-              <OurProcess text="Team match-up" />
-              <OurProcess sideImage text="Project implementation" />
-              <OurProcess sideImage text="Effect analysis" />
-              <OurProcess text="Long-term relationship!" />
-            </div>
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Seo title="Home" description="Welcome to GatsbyJs v1" />
+      <HomeHero />
+      <Work />
+      <section className="section">
+        <div className="container">
+          <div className="columns is-multiline">
+            <OurProcess text="Requirement Analysis" sideImage />
+            <OurProcess sideImage text="Project Allocation" />
+            <OurProcess text="Team match-up" />
+            <OurProcess sideImage text="Project implementation" />
+            <OurProcess sideImage text="Effect analysis" />
+            <OurProcess text="Long-term relationship!" />
           </div>
-        </section>
-        <SocialStuff />
-        <NewsAndUpdates />
-      </Layout>
-    );
-  }
-}
+        </div>
+      </section>
+      <SocialStuff />
+      <NewsAndUpdates />
+    </Layout>
+  );
+};
+export default IndexPage;
