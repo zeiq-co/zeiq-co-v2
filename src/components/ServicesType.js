a93d9d8b1502938c93b45734bd3c8f138de5805f
dharmveer97 <dvbangar8@gmail.com>
2020-05-28 17:07:21 +0530

    minor improvements.

--- src/components/ServicesType.js
+++ src/components/ServicesType.js
@@ -57,8 +57,8 @@ const ServicesType = ({ flex }) => {
           </figure>
         </div>
         <div className="column is-6">
-          <h2 className="title is-2 ">Mobile Apps development</h2>
-          <p className="subtitle is-6 is-spaced">
+          <h2 className="title is-2 is-spaced">Mobile Apps development</h2>
+          <p className="subtitle is-6">
             A successful offshore software application development company since
             1997, providing a full range of website and mobile app development ,
             and other IT services and solutions globally. We deliver web and
