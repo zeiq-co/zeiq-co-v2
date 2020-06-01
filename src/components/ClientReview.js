2039fc1f826045b8c58b316e9bde7dd6ca254695
dharmveer97 <dvbangar8@gmail.com>
2020-06-01 18:47:38 +0530

    minor fixes.

--- src/components/ClientReview.js
+++ src/components/ClientReview.js
@@ -1,63 +1,107 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Slider from 'react-slick';
 import styled from 'styled-components';
 import Heading from './elements/Heading';
 
+const Section = styled.div`
+  .slick-slide {
+    padding: 0 8px;
+    box-sizing: border-box;
+  }
+  .slick-dots {
+    bottom: -50px !important;
+  }
+  .slick-dots li button:before {
+    color: ${props => props.theme.mainBrandColor} !important;
+    font-size: 15px;
+    line-height: 20px;
+  }
+  .wrapper {
+    :focus {
+      outline: 0;
+      outline: none;
+    }
+  }
+  .card {
+    border: 2px solid ${props => props.theme.borderColor} !important;
+    border-radius: 8px;
+    box-shadow: none;
+  }
+`;
 const data = [
   {
-    text: 'Requirement Analysis',
+    position: 'Project Manager',
+    name: 'Zachary Frank',
+    content:
+      'For business professionals caught between high OEM price and mediocre print and graphic output, there’s a solution: Business Express’s Eclipse line of compatible laser toner cartridges that meet or exceed ',
   },
   {
-    text: 'Project Allocation',
+    position: 'Project Manager',
+    name: 'Lenora Andrews',
+    content:
+      'There is a lot of exciting stuff going on in the stars above us that make astronomy so much fun. The truth is the universe is a constantly changing, moving, some would say “living” thing because you just never know what you.',
   },
   {
-    text: 'Team match-up',
+    position: 'Project Manager',
+    name: 'Francis Morgan',
+    content:
+      'While it was just a TV show, that little speech at the beginning of the original Star Trek show really did do a good job of capturing our feelings about space. It is those feelings that drive our love of astronomy and our desire.',
+  },
+  {
+    position: 'Project Manager',
+    name: 'Lenora Andrews',
+    content:
+      'For business professionals caught between high OEM price and mediocre print and graphic output, there’s a solution: Business Express’s Eclipse line of compatible laser toner cartridges that meet or exceed ',
+  },
+  {
+    position: 'Project Manager',
+    name: 'Francis Morgan',
+    content:
+      'For business professionals caught between high OEM price and mediocre print and graphic output, there’s a solution: Business Express’s Eclipse line of compatible laser toner cartridges that meet or exceed ',
   },
 ];
 
-const Container = styled.div`
-  /* p {
-    color: ${props => props.theme.textColor};
-  } */
-`;
-
-const ClientReview = () => {
-  return (
-    <Container className="section">
-      <div className="container">
-        <Heading title="Words From Clients" />
-        <div className="columns is-multiline">
-          {data.map(item => (
-            <div className="column is-4">
-              <div className="card">
-                <div className="card-content">
-                  <p className="subtitle is-6">
-                    For business professionals caught between high OEM price and
-                    mediocre print and graphic output, there’s a solution:
-                    Business Express’s Eclipse line of compatible laser toner
-                    cartridges that meet or exceed
-                  </p>
-                  <div className="media">
-                    <div className="media-left">
-                      <figure className="image is-48x48">
-                        <img
-                          className="is-rounded"
-                          src="/images/userIcon.png"
-                          alt="Place"
-                        />
-                      </figure>
-                    </div>
-                    <div className="media-content">
-                      <p className="title is-6">John Smith</p>
-                      <p className="subtitle is-7">@johnsmith</p>
+export default class MultipleItems extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+    };
+    return (
+      <Section className="section">
+        <div className="container">
+          <Heading title="Words From Clients" />
+          <Slider {...settings}>
+            {data.map(item => (
+              <div className="wrapper">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="subtitle is-6">{item.content}</p>
+                    <div className="media">
+                      <div className="media-left">
+                        <figure className="image is-48x48">
+                          <img
+                            className="is-rounded"
+                            src="/images/userIcon.png"
+                            alt="Place"
+                          />
+                        </figure>
+                      </div>
+                      <div className="media-content">
+                        <p className="title is-6">{item.name}</p>
+                        <p className="subtitle is-7">{item.position}</p>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </Slider>
         </div>
-      </div>
-    </Container>
-  );
-};
-export default ClientReview;
+      </Section>
+    );
+  }
+}
