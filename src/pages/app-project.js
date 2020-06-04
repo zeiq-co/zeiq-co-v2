2787ccf1e970f4899782bc6fbc97c95c90f003ca
dharmveer97 <dvbangar8@gmail.com>
2020-06-04 16:23:23 +0530

    minor fixes.

--- src/pages/app-project.js
+++ src/pages/app-project.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
-// import Heading from '../components/elements/Heading';
 import HomeHero from '../components/HomeHero';
 import AppSlider from '../components/AppSlider';
+import ContactUs from '../components/ContactUs';
 
 const Project = () => {
   return (
@@ -16,14 +16,8 @@ const Project = () => {
         title="Food On Mood"
         subtitle="Food On Mood is an online food and app that facilitates online shopping for the day-to-day needs of the customers."
       />
-
-      {/* <Heading
-        columnSize
-        title="About Project"
-        subtitle="Food On Mood is a personal assistant to meet your everyday needs of online supermarket. At Mumu, customer can select nearby stores available, and order things as per their needs. The customer can also get alternative products in case the desired item is not available in the store. A Mover goes to the store and buys the ordered items and delivers on a given location at the specified delivery time."
-      /> */}
-
       <AppSlider />
+      <ContactUs />
     </Layout>
   );
 };
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,48 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import Seo from '../components/Seo';
 import Layout from '../components/Layout';
 import config from '../utils/config';
-import ContactForm from '../components/form/ContactForm';
-
-const Container = styled.section`
-  margin-top: -1px;
-`;
-
-const Mail = styled.a`
-  color: ${props => props.theme.mainBrandColor} !important;
-  margin-bottom: 37px !important;
-`;
-
-const H2 = styled.h2`
-  margin-bottom: 7.6px !important;
-`;
-const MailWrapper = styled.div`
-  margin-bottom: 37px !important;
-  @media only screen and (min-width: 768px) {
-    p {
-      padding: 0px 8rem 0px 0px;
-    }
-  }
-`;
-
-const Country = styled.div`
-  margin-bottom: 36px;
-  img {
-    margin-right: 14px;
-    height: 20px;
-  }
-  span {
-    width: 3rem !important;
-  }
-`;
-
-const Socials = styled.div`
-  span {
-    margin-right: 10px;
-  }
-`;
+import ContactUs from '../components/ContactUs';
 
 const ContactPage = () => {
   return (
@@ -52,95 +13,7 @@ const ContactPage = () => {
         description={`Contact us ${config.siteName}`}
         url={`${config.siteUrl}/Contact`}
       />
-      <Container className="section">
-        <div className="container">
-          <div className="columns is-mobile is-centered">
-            <div className="column is-four-fifths">
-              <div className="columns is-vcentered">
-                <div className="column is-6">
-                  <section className="hero">
-                    <div className="hero-body">
-                      <h1 className="title is-2 has-text-black">
-                        Get in touch
-                      </h1>
-                      <p className="subtitle is-6">
-                        When you enter into any new area of science you almost
-                        always find yourself.
-                      </p>
-                      <ContactForm />
-                    </div>
-                  </section>
-                </div>
-                <div className="column">
-                  <section className="hero">
-                    <div className="hero-body">
-                      <MailWrapper>
-                        <H2 className="is-size-7 has-text-weight-bold has-text-black is-uppercase">
-                          EMAIL
-                        </H2>
-                        <Mail href="mailto:hi@zeiq.co">hi@zeiq.co</Mail>
-                      </MailWrapper>
-                      <H2 className="is-size-7 has-text-weight-bold has-text-black is-uppercase">
-                        Phone
-                      </H2>
-                      <Country>
-                        <div className="main">
-                          <a href="tel:+91-8847411956">
-                            <span className="icon">
-                              <img src="/images/india.png" alt="" />
-                            </span>
-                            +91-8847411956
-                          </a>
-                        </div>
-                        <div className="main">
-                          <a href="tel:01582-888653">
-                            <span className="icon">
-                              <img src="/images/uk.png" alt="" />
-                            </span>
-                            01582-888653
-                          </a>
-                        </div>
-                      </Country>
-                      <MailWrapper>
-                        <H2 className="is-size-7 has-text-weight-bold has-text-black is-uppercase">
-                          Address
-                        </H2>
-                        <p>Guru Amadas Nagar, Salempur Road, Jalandhar</p>
-                        <p>38 Golf Road, Swineshead Bridge, England </p>
-                      </MailWrapper>
-                      <Socials>
-                        <H2 className="is-size-7 has-text-weight-bold has-text-black is-uppercase">
-                          socials
-                        </H2>
-                        <a href="">
-                          <span className="icon has-text-grey-light is-size-4">
-                            <i className="fab fa-facebook-square" />
-                          </span>
-                        </a>
-                        <a href="">
-                          <span className="icon has-text-grey-light is-size-4">
-                            <i className="fab fa-google-plus-g" />
-                          </span>
-                        </a>
-                        <a href="">
-                          <span className="icon has-text-grey-light is-size-4">
-                            <i className="fab fa-twitter" />
-                          </span>
-                        </a>
-                        <a href="">
-                          <span className="icon has-text-grey-light is-size-4">
-                            <i className="fab fa-instagram" />
-                          </span>
-                        </a>
-                      </Socials>
-                    </div>
-                  </section>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Container>
+      <ContactUs />
     </Layout>
   );
 };
