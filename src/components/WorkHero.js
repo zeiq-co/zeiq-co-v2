bc44803a66a56bcf6f20c3940dd98ae397345ff6
dharmveer97 <dvbangar8@gmail.com>
2020-06-05 13:16:52 +0530

    minor fixes.

--- src/components/WorkHero.js
+++ src/components/WorkHero.js
@@ -7,28 +7,32 @@ const Header = styled.div`
   }
 `;
 
+const Section = styled.div`
+  @media only screen and (min-width: 1023px) {
+    padding: 3rem 5rem !important;
+  }
+`;
+
 const WorkHero = () => {
   return (
     <section className="columns is-vcentered">
-      <div className="column is-6 ">
+      <div className="column is-6">
         <figure className="image is-3by2">
-          <img src="/images/work/worked.png" alt="" />
+          <img src="/images/work/worked.png" alt="work" />
         </figure>
       </div>
-      <div className="column is-6 hero ">
-        <div className="hero-body">
-          <Header>
-            <h1 className="title is-spaced is-1">
-              Projects that speak about our versatility
-            </h1>
-            <p className="">
-              We’re passionate about what we do! Our portfolio is a sheer
-              presentation about 20+ years of industry experience,
-              collaboration, expertise, and quality deliverables.
-            </p>
-          </Header>
-        </div>
-      </div>
+      <Section className="column is-6 section">
+        <Header>
+          <h1 className="title is-spaced is-1">
+            Projects that speak about our versatility
+          </h1>
+          <p className="">
+            We’re passionate about what we do! Our portfolio is a sheer
+            presentation about 20+ years of industry experience, collaboration,
+            expertise, and quality deliverables.
+          </p>
+        </Header>
+      </Section>
     </section>
   );
 };
