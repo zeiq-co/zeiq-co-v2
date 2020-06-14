80f4e291dad25e5d493a1e501d5912e791774331
dharmveer97 <dvbangar8@gmail.com>
2020-06-14 17:00:28 +0530

    add new page

---
+++ src/pages/single-service.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import Seo from '../components/Seo';
+import Heading from '../components/elements/Heading';
+import ServicesType from '../components/ServicesType';
+import Tabs from '../components/Tabs';
+
+const SingleService = () => {
+  return (
+    <Layout>
+      <Seo title="Services" description="Our Services" />
+      <div className="section">
+        <div className="container">
+          <Heading
+            columnSize
+            title="Web Development Company"
+            subtitle="A successful offshore software application development company since 1997, providing a full range of website and mobile app development , and other IT services and solutions globally."
+          />
+          <ServicesType image="/images/services/serviceOne.png" />
+          <ServicesType image="/images/services/serviceTwo.png" flex />
+          <ServicesType image="/images/services/service3rd.png" />
+          <ServicesType image="/images/services/serviceTwo.png" flex />
+        </div>
+      </div>
+      <div className="section">
+        <Heading title="Technologies we work with" />
+        <div className="container">
+          <Tabs />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+export default SingleService;
