86469dac871b158ed7d165d72dcbb08708db0b52
dharmveer97 <dvbangar8@gmail.com>
2020-06-17 14:43:31 +0530

    minor fixes.

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,18 +22,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-169811954-1',
-        head: false,
-        anonymize: true,
-        respectDNT: true,
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        pageTransitionDelay: 0,
-        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-        defer: false,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'commbus.netlify.app/',
       },
     },
     {
