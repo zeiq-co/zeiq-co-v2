0785c2027bcae995278fafce1f256e0fe7f0aef2
dharmveer97 <dvbangar8@gmail.com>
2020-06-19 15:34:00 +0530

    minor fixes.

--- src/components/ServicesType.js
+++ src/components/ServicesType.js
@@ -29,7 +29,29 @@ const Points = styled.div`
   }
 `;
 
-const ServicesType = ({ flex, title, image, description, features }) => {
+const features = [
+  {
+    feature: 'Native iOS App Development',
+  },
+  {
+    feature: 'Native Android App Development',
+  },
+  {
+    feature: 'Cross-platform App Development',
+  },
+  {
+    feature: 'Feature Five',
+  },
+  {
+    feature: 'Wearable App Development',
+  },
+
+  {
+    feature: 'Feature Six',
+  },
+];
+
+const ServicesType = ({ flex, title, image, description }) => {
   return (
     <Container flex={flex}>
       <div className="columns is-multiline  is-variable is-8">
@@ -37,8 +59,17 @@ const ServicesType = ({ flex, title, image, description, features }) => {
           <img src={image} alt="mobile" />
         </div>
         <div className="column">
-          <h2 className="title is-2 is-spaced">{title}</h2>
-          <p className="subtitle is-6">{description}</p>
+          <h2 className="title is-2 is-spaced">Mobile Apps Development </h2>
+          <p className="subtitle is-6">
+            From B2B or B2E apps for enterprises, small businesses, and
+            startups, we have a proven track record of offering high-impact,
+            result driven and engaging mobile application development services.
+            Zeiq is a top mobile app development company that is renowned for
+            delivering native Android app development, native iOS app
+            development, hybrid and cross-platform app development services to
+            build next-gen mobile applications using the latest technology
+            stack.
+          </p>
           <div>
             <h2 className="title is-4">Features</h2>
           </div>
