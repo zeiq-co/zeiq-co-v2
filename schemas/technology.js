eb1d61e1a530dec5c2c75cd712efd2ec3ae346a3
dharmveer97 <dvbangar8@gmail.com>
2020-06-30 15:37:37 +0530

    add tabs

--- schemas/technology.js
+++ schemas/technology.js
@@ -2,75 +2,47 @@ import Tabs from 'sanity-plugin-tabs';
 
 export default {
   type: 'document',
-  title: `Frontpage`,
-  name: `frontpage`,
+  title: `Technology`,
+  name: `technology`,
+  inputComponent: Tabs,
+  fieldsets: [
+    { name: 'mobile', title: 'Mobile' },
+    { name: 'frontEnd', title: 'Front End' },
+    { name: 'dataBase', title: 'Database' },
+    { name: 'backEnd', title: 'BackEnd' },
+    { name: 'cms', title: 'Cms' },
+  ],
+
   fields: [
     {
-      name: 'content',
-      type: 'object',
-      inputComponent: Tabs,
-
-      fieldsets: [
-        { name: 'main', title: 'Main' },
-        { name: 'aside', title: 'Aside' },
-        { name: 'meta', title: 'Meta' },
-      ],
-      options: {
-        layout: 'object',
-      },
-
-      fields: [
-        {
-          type: 'object',
-          name: 'mainTitle',
-          title: 'Main Title',
-          fieldset: 'main',
-          fields: [
-            {
-              type: 'string',
-              name: 'header',
-              title: 'Header',
-            },
-            {
-              type: 'image',
-              name: 'ingressText',
-              title: 'Text',
-            },
-          ],
-        },
-        {
-          type: 'string',
-          name: 'info',
-          title: 'Information',
-          fieldset: 'aside',
-        },
-        {
-          type: 'object',
-          name: 'aside',
-          fieldset: 'meta',
-          inputComponent: Tabs,
-
-          fieldsets: [
-            { name: 'tags', title: 'Tags' },
-            { name: 'categories', title: 'Categories' },
-          ],
-
-          fields: [
-            {
-              type: 'string',
-              name: 'contentType',
-              title: 'Content Type',
-              fieldset: 'tags',
-            },
-            {
-              type: 'string',
-              name: 'category',
-              title: 'Category',
-              fieldset: 'categories',
-            },
-          ],
-        },
-      ],
+      type: 'tab',
+      name: 'mobile',
+      title: 'Mobile',
+      fieldset: 'mobile',
+    },
+    {
+      type: 'tab',
+      name: 'frontEnd',
+      title: 'FrontEnd',
+      fieldset: 'frontEnd',
+    },
+    {
+      type: 'tab',
+      name: 'dataBase',
+      title: 'DataBase',
+      fieldset: 'dataBase',
+    },
+    {
+      type: 'tab',
+      name: 'backEnd',
+      title: 'Back End',
+      fieldset: 'backEnd',
+    },
+    {
+      type: 'tab',
+      name: 'cms',
+      title: 'Cms',
+      fieldset: 'backEnd',
     },
   ],
 };
